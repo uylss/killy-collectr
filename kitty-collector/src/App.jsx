98e03ce1d70bc805befc32ec654feb269885d062
gaelg311 <gael@pdx.edu>
2025-01-24 02:53:05 -0800

    Created navabr component and style sheet

--- kitty-collector/src/App.jsx
+++ kitty-collector/src/App.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import "./assets/css/styles.css";
+import Navbar from "./assets/components/navbar";
+
 export default function App() {
-  return "app";
+  return <Navbar />;
 }
---
+++ kitty-collector/src/assets/components/navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">Gallery</li>
+        <li className="navbar-item">Collection</li>
+        <li className="navbar-item">Open Packs</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
